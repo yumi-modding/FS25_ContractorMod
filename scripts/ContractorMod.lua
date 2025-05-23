95f3952f25515ba48bc79c01c8c51990e7d2a161
Yumi Modding <yumi@yumi-modding.fr>
2025-05-23 21:46:50 +0200

    First commit basically functionnal

---
+++ scripts/ContractorMod.lua
@@ -0,0 +1,342 @@
+source(Utils.getFilename("scripts/ContractorModWorker.lua", g_currentModDirectory))
+
+ContractorMod = {}
+ContractorMod.debug = false --true --
+
+-- Create multiple players and add them to the PlayerSystem
+--local player1 = Player.new(...)
+--local player2 = Player.new(...)
+--g_playerSystem:addPlayer(player1)
+--g_playerSystem:addPlayer(player2)
+
+
+-- Switch control
+-- g_playerSystem:setLocalPlayer(player2) -- Now you control player2
+
+function ContractorMod:init()
+  if ContractorMod.debug then print("ContractorMod:init()") end
+    self.currentID = 1
+    self.numWorkers = 2
+    self.workers = {}
+    ContractorMod.displayPlayerNames = true
+
+    local savegameDir;
+    if g_currentMission.missionInfo.savegameDirectory then
+      savegameDir = g_currentMission.missionInfo.savegameDirectory;
+    end;
+    if not savegameDir and g_careerScreen.currentSavegame and g_careerScreen.currentSavegame.savegameIndex then
+      savegameDir = ('%ssavegame%d'):format(getUserProfileAppPath(), g_careerScreen.currentSavegame.savegameIndex);
+    end;
+    if not savegameDir and g_currentMission.missionInfo.savegameIndex ~= nil then
+      savegameDir = ('%ssavegame%d'):format(getUserProfileAppPath(), g_careerScreen.missionInfo.savegameIndex);
+    end;
+    self.savegameFolderPath = savegameDir;
+    self.ContractorModXmlFilePath = self.savegameFolderPath .. '/ContractorMod.xml';
+
+    -- Try to load from savegame, else create default workers
+    if not self:initFromSave() then
+        table.insert(self.workers, ContractorModWorker:new("Alex", 1, g_helperManager:getRandomHelperStyle()))
+        table.insert(self.workers, ContractorModWorker:new("Brenda", 2, g_helperManager:getRandomHelperStyle()))
+        table.insert(self.workers, ContractorModWorker:new("Chris", 3, g_helperManager:getRandomHelperStyle()))
+        table.insert(self.workers, ContractorModWorker:new("David", 4, g_helperManager:getRandomHelperStyle()))
+        self.numWorkers = #self.workers
+        self.displaySettings = {}
+        self.displaySettings.characterName = {}
+        self.displaySettings.characterName.x = 0.9828
+        self.displaySettings.characterName.y = 0.90
+        self.displaySettings.characterName.size = 0.024
+    end
+end
+
+function ContractorMod:initFromSave()
+  if ContractorMod.debug then print("ContractorMod:initFromSave()") end
+    -- Minimal: load worker name, position, style from XML
+    local savePath = g_currentMission.missionInfo.savegameDirectory .. "/ContractorMod.xml"
+    if not fileExists(savePath) then return false end
+    local xml = XMLFile.load("ContractorMod", savePath)
+    local num = xml:getInt("ContractorMod.workers#numWorkers") or 0
+    for i = 1, num do
+        local key = string.format("ContractorMod.workers.worker(%d)", i-1)
+        local name = xml:getString(key.."#name")
+        local pos = xml:getString(key.."#position")
+        local rot = xml:getString(key.."#rotation")
+        local style = PlayerStyle.new()
+        -- style:loadFromXMLFile(xml, key..".style")
+        local worker = ContractorModWorker:new(name, i, style)
+        if pos then worker.x, worker.y, worker.z = string.getVector(pos) end
+        if rot then worker.rotX, worker.rotY = string.getVector(rot) end
+        local vehicleID = xml:getString(key.."#vehicleID");
+        if vehicleID ~= "0" then
+          if ContractorMod.mapVehicleLoad ~= nil then
+            -- map savegame vehicle id and network id
+            local saveId = ContractorMod.mapVehicleLoad[vehicleID]
+            local vehicle = NetworkUtil.getObject(tonumber(saveId))
+            if vehicle ~= nil then
+              if ContractorMod.debug then print("ContractorMod: vehicle not nil") end
+              worker.currentVehicle = vehicle
+              local currentSeat = xml:getInt(key.."#currentSeat");
+              if currentSeat ~= nil then
+                worker.currentSeat = currentSeat
+              end
+            end
+          end
+        end
+        table.insert(self.workers, worker)
+    end
+    xmlKey = "ContractorMod.displaySettings.characterName"
+    self.displaySettings = {}
+    self.displaySettings.characterName = {}
+    local x = xml:getFloat(xmlKey .. string.format("#x"));
+    if x == nil then
+      x = 0.9828
+    end
+    self.displaySettings.characterName.x = x
+    local y = xml:getFloat(xmlKey .. string.format("#y"));
+    if y == nil then
+      y = 0.90
+    end
+    self.displaySettings.characterName.y = y
+    local size = xml:getFloat(xmlKey .. string.format("#size"));
+    if size == nil then
+      size = 0.024
+    end
+    self.displaySettings.characterName.size = size
+    xmlKey = "ContractorMod.displaySettings.playerName"
+    ContractorMod.displayPlayerNames = Utils.getNoNil(xml:getBool(xmlKey .. string.format("#displayPlayerNames")), true);
+    self.numWorkers = #self.workers
+    xml:delete()
+    return self.numWorkers > 0
+end
+
+-- @doc Will call dedicated save method
+SavegameController.onSaveComplete = Utils.prependedFunction(SavegameController.onSaveComplete, function(self)
+    -- if self.isValid and self.xmlKey ~= nil then
+    ContractorMod:onSaveCareerSavegame()
+    -- end
+end);
+
+-- @doc store savegame vehicle id if worker is in this vehicle
+function ContractorMod:mapVehicleSave(vehicle, saveId)
+  if ContractorMod.debug then print("ContractorMod:mapVehicleSave ") end
+  if self.workers ~= nil then
+    if #self.workers > 0 then
+      for i = 1, self.numWorkers do
+        local worker = self.workers[i]
+        if worker ~= nil and worker.currentVehicle ~= nil then
+          if vehicle == worker.currentVehicle then
+            -- store savegame vehicle id 
+            worker.saveId = saveId
+          end
+        end
+      end
+    end
+  end
+end
+
+-- @doc Set mapping between savegame vehicle id and vehicle network id when vehicle is saved
+function ContractorMod:preVehicleSave(xmlFile, key, usedModNames)
+  if ContractorMod.debug then print("ContractorMod:preVehicleSave ") end
+  -- key is something like vehicles.vehicle(saveId)
+  local saveId = 1 + tonumber(string.sub(key, string.find(key, '(', 1, true) + 1, string.find(key, ')', 1, true) - 1))
+  if SpecializationUtil.hasSpecialization(Enterable, self.specializations) then
+    ContractorMod:mapVehicleSave(self, tostring(saveId))
+  end
+end
+Vehicle.saveToXMLFile = Utils.prependedFunction(Vehicle.saveToXMLFile, ContractorMod.preVehicleSave);
+
+-- @doc Save workers info to restore them when starting game
+function ContractorMod:onSaveCareerSavegame()
+  if ContractorMod.debug then print("ContractorMod:onSaveCareerSavegame ") end
+  if self.workers ~= nil then
+    local xmlFile;
+    if fileExists(self.ContractorModXmlFilePath) then
+      xmlFile = XMLFile.load('ContractorMod', self.ContractorModXmlFilePath);
+    else
+      xmlFile = XMLFile.create('ContractorMod', self.ContractorModXmlFilePath, 'ContractorMod');
+      xmlFile:save()
+    end;
+
+    if xmlFile ~= nil then
+      local rootXmlKey = "ContractorMod"
+
+      -- update current worker position
+      local currentWorker = self.workers[self.currentID]
+      if currentWorker ~= nil then
+        currentWorker:beforeSwitch(true)
+      end
+      
+      local workerKey = rootXmlKey .. ".workers"
+      xmlFile:setInt(workerKey.."#numWorkers", self.numWorkers);
+      for i = 1, self.numWorkers do
+        local worker = self.workers[i]
+        local key = string.format(rootXmlKey .. ".workers.worker(%d)", i - 1);
+        xmlFile:setString(key.."#name", worker.name);
+        local pos = worker.x..' '..worker.y..' '..worker.z
+        xmlFile:setString(key.."#position", pos);
+        local rot = worker.dx..' '..worker.dy..' '..worker.dz
+        xmlFile:setString(key.."#rotation", rot);
+        local vehicleID = "0"
+        if worker.currentVehicle ~= nil then
+          vehicleID = worker.saveId
+        end
+        xmlFile:setString(key.."#vehicleID", vehicleID);
+      end
+      -- currentWorker.player:moveToAbsoluteInternal(0, -200, 0);
+      local xmlKey = rootXmlKey .. ".displaySettings.characterName"
+      xmlFile:setFloat(xmlKey .. "#x", self.displaySettings.characterName.x);
+      xmlFile:setFloat(xmlKey .. "#y", self.displaySettings.characterName.y);
+      xmlFile:setFloat(xmlKey .. "#size", self.displaySettings.characterName.size);
+      xmlKey = rootXmlKey .. ".displaySettings.playerName"
+      xmlFile:setBool(xmlKey .. "#displayPlayerNames", ContractorMod.displayPlayerNames);
+      xmlFile:save()
+      xmlFile:delete()
+    end
+  end
+end
+
+function ContractorMod:saveToXML()
+  if ContractorMod.debug then print("ContractorMod:saveToXML()") end
+    local savePath = g_currentMission.missionInfo.savegameDirectory .. "/ContractorMod.xml"
+    local xml = XMLFile.create("ContractorMod", savePath, "ContractorMod")
+    xml:setInt("ContractorMod.workers#numWorkers", self.numWorkers)
+    for i, worker in ipairs(self.workers) do
+        local key = string.format("ContractorMod.workers.worker(%d)", i-1)
+        xml:setString(key.."#name", worker.name)
+        xml:setString(key.."#position", string.format("%f %f %f", worker.x, worker.y, worker.z))
+        xml:setString(key.."#rotation", string.format("%f %f", worker.rotX, worker.rotY))
+        -- worker.playerStyle:saveToXMLFile(xml, key..".style")
+    end
+    xml:save()
+    xml:delete()
+end
+-- GameSettings.saveToXMLFile = Utils.prependedFunction(GameSettings.saveToXMLFile, ContractorMod.saveToXML)
+
+-- @doc Set mapping between savegame vehicle id and vehicle network id when vehicle is loaded
+ContractorMod.appEnterableOnLoad = function(self, savegame)
+  if ContractorMod.debug then print("ContractorMod:appEnterableOnLoad ") end
+  if savegame ~= nil then
+    -- When loading savegame
+    if ContractorMod.mapVehicleLoad == nil then
+      ContractorMod.mapVehicleLoad = {}
+    end
+    local key = savegame.key
+    -- key is something like vehicles.vehicle(saveId)
+    local saveId = 1 + tonumber(string.sub(key, string.find(key, '(', 1, true) + 1, string.find(key, ')', 1, true) - 1))
+    local vehicleID = self.id
+    -- Set mapping between savegame vehicle id and vehicle network id once loaded
+    ContractorMod.mapVehicleLoad[tostring(saveId)] = vehicleID
+    -- DebugUtil.printTableRecursively(ContractorMod.mapVehicleLoad, " ", 1, 2);
+  end
+end
+Enterable.onLoad = Utils.appendedFunction(Enterable.onLoad, ContractorMod.appEnterableOnLoad)
+
+
+-- @doc Prevent from removing driver character
+function ContractorMod:replaceGetDisableVehicleCharacterOnLeave(superfunc)
+  if ContractorMod.debug then print("ContractorMod:replaceGetDisableVehicleCharacterOnLeave ") end
+  if ContractorMod.switching then
+    ContractorMod.switching = false
+    if ContractorMod.debug then print("switching return false") end
+    return false
+  end
+  if ContractorMod.passengerLeaving then
+    ContractorMod.passengerLeaving = false
+    if ContractorMod.debug then print("passengerLeaving return false") end
+    return false
+  end
+  return true
+end
+Enterable.getDisableVehicleCharacterOnLeave = Utils.overwrittenFunction(Enterable.getDisableVehicleCharacterOnLeave, ContractorMod.replaceGetDisableVehicleCharacterOnLeave);
+
+function ContractorMod:switchWorker(newID)
+  if ContractorMod.debug then print("ContractorMod:switchWorker() " + string(newID)) end
+    if newID == self.currentID or newID < 1 or newID > self.numWorkers then return end
+    self.workers[self.currentID]:beforeSwitch()
+    self.currentID = newID
+    self.workers[self.currentID]:afterSwitch()
+end
+
+-- Hook into game events as needed (e.g., onStartMission, onSaveGame)
+Mission00.onStartMission = Utils.appendedFunction(Mission00.onStartMission, function()
+    ContractorMod:init()
+end)
+
+
+function ContractorMod:onSwitchVehicle(action)
+  if ContractorMod.debug then print("ContractorMod:onSwitchVehicle()") end
+  self.switching = true
+  if action == "SWITCH_VEHICLE" then
+    if ContractorMod.debug then print('ContractorMod_NEXTWORKER pressed') end
+    local nextID = 0
+    if ContractorMod.debug then print("ContractorMod: self.currentID " .. tostring(self.currentID)) end
+    if ContractorMod.debug then print("ContractorMod: self.numWorkers " .. tostring(self.numWorkers)) end
+    if self.currentID < self.numWorkers then
+      nextID = self.currentID + 1
+    else
+      nextID = 1
+    end
+    if ContractorMod.debug then print("ContractorMod: nextID " .. tostring(nextID)) end
+    self:setCurrentContractorModWorker(nextID)
+  elseif action == "SWITCH_VEHICLE_BACK" then
+    if ContractorMod.debug then print('ContractorMod_PREVWORKER pressed') end
+    local prevID = 0
+    if self.currentID > 1 then
+      prevID = self.currentID - 1
+    else
+      prevID = self.numWorkers
+    end    
+    self:setCurrentContractorModWorker(prevID)
+  end
+end
+
+-- @doc Change active worker
+function ContractorMod:setCurrentContractorModWorker(setID)
+  if ContractorMod.debug then print("ContractorMod:setCurrentContractorModWorker(setID) " .. tostring(setID) .. " - " .. tostring(self.currentID)) end
+  local currentWorker = self.workers[self.currentID]
+  if currentWorker ~= nil then
+    self.shouldStopWorker = false
+    self.switching = true
+    currentWorker:beforeSwitch()
+  end
+  self.currentID = setID
+  currentWorker = self.workers[self.currentID]
+  if currentWorker ~= nil then
+    currentWorker:afterSwitch()
+    self.shouldStopWorker = true
+    self.switching = false
+  end
+  --DebugUtil.printTableRecursively(self.workers, " ", 1, 3)
+end
+
+-- @doc Replace switch vehicle by switch worker
+function ContractorMod:replaceOnSwitchVehicle(superfunc, action, direction)
+  ContractorMod:onSwitchVehicle(action)
+end
+PlayerInputComponent.onInputSwitchVehicle = Utils.overwrittenFunction(PlayerInputComponent.onInputSwitchVehicle, ContractorMod.replaceOnSwitchVehicle);
+
+function ContractorMod:preventOnSwitchVehicle(superfunc, value, direction)
+  if ContractorMod.debug then print("ContractorMod:preventOnSwitchVehicle()") end
+  print("We might prevent to switch vehicle from the map menu")
+  superfunc(self, value, direction)
+end
+InGameMenuMapFrame.onSwitchVehicle = Utils.overwrittenFunction(InGameMenuMapFrame.onSwitchVehicle, ContractorMod.preventOnSwitchVehicle);
+
+function ContractorMod:loadedMission() --[[----------------------------------------------------------------]] print("This is a development version of ContractorMod for FS25, which may and will contain errors, bugs.") end
+Mission00.loadMission00Finished = Utils.appendedFunction(Mission00.loadMission00Finished, ContractorMod.loadedMission)
+
+-- @doc Draw worker name and hotspots on map
+function ContractorMod:draw()
+  --if ContractorModWorker.debug then print("ContractorMod:draw()") end
+  --Display current worker name
+  if self.workers ~= nil then
+    if #self.workers > 0 and g_currentMission.hud.isVisible then
+      local currentWorker = self.workers[self.currentID]
+      if currentWorker ~= nil then
+        --Display current worker name
+        currentWorker:displayName(self)
+      end
+    end
+  end
+end
+
+addModEventListener(ContractorMod);
---
+++ scripts/ContractorModWorker.lua
@@ -0,0 +1,181 @@
+--
+-- ContractorMod
+-- Specialization for storing each character data
+-- No event plugged, only called when interacting with ContractorMod
+--
+-- @author  yumi
+-- free for noncommercial-usage
+--
+
+ContractorModWorker = {};
+ContractorModWorker_mt = Class(ContractorModWorker);
+
+ContractorModWorker.debug = false --true --
+
+function ContractorModWorker:getParentComponent(node)
+  return self.graphicsRootNode;
+end;
+
+function ContractorModWorker:new(name, index, workerStyle)
+  if ContractorModWorker.debug then print("ContractorModWorker:new()") end
+  local self = {};
+  setmetatable(self, ContractorModWorker_mt);
+
+  self.name = name
+  self.currentVehicle = nil
+
+
+  self.color = Farm.COLORS[index]
+  if g_localPlayer ~= nil then
+    self.x, self.y, self.z, self.rotY = g_localPlayer:getPosition()
+    self.dx, self.dy, self.dz = localDirectionToWorld(g_localPlayer.rootNode, 0, 0, 1);
+    self.rotX = 0.;
+    -- self.rotY = 0.73;
+    self.x = self.x + (1 * index)
+  end
+  return self
+end
+
+
+function ContractorModWorker:displayName(contractorMod)
+  --if ContractorModWorker.debug then print("ContractorModWorker:displayName()") end
+  if self.name == "PLAYER" then return end
+  setTextBold(true);
+  setTextAlignment(RenderText.ALIGN_RIGHT);
+  
+  setTextColor(self.color[1], self.color[2], self.color[3], 1.0);
+  local x = 0.9828
+  local y = 0.45
+  local size = 0.024
+  if contractorMod.displaySettings ~= nil and contractorMod.displaySettings.characterName ~= nil then
+    x = contractorMod.displaySettings.characterName.x
+    y = contractorMod.displaySettings.characterName.y
+    size = contractorMod.displaySettings.characterName.size
+  end
+  renderText(x, y, size, self.name);
+  
+  if ContractorModWorker.debug then
+    if self.currentVehicle ~= nil then
+      local vehicleName = ""
+      if self.currentVehicle ~= nil then
+        vehicleName = self.currentVehicle:getFullName()
+      end
+      renderText(0.9828, 0.43, 0.012, vehicleName);
+      renderText(0.9828, 0.42, 0.012, "seat:" .. tostring(self.currentSeat) );
+    end
+    renderText(0.9828, 0.41, 0.012, self.name);
+    renderText(0.9828, 0.40, 0.012, "x:" .. tostring(self.x) .. " y:" .. tostring(self.y) .. " z:" .. tostring(self.z));
+    renderText(0.9828, 0.39, 0.012, "dx:" .. tostring(self.dx) .. " dy:" .. tostring(self.dy) .. " dz:" .. tostring(self.dz));
+    renderText(0.9828, 0.38, 0.012, "rotX:" .. tostring(self.rotX) .. " rotY:" .. tostring(self.rotY));
+    -- renderText(0.9828, 0.37, 0.012, "graphicsRotY:" .. tostring(self.player.graphicsRotY));
+    -- renderText(0.9828, 0.36, 0.012, "targetGraphicsRotY:" .. tostring(self.player.targetGraphicsRotY));
+    renderText(0.9828, 0.35, 0.012, "shouldStopWorker:  " .. tostring(contractorMod.shouldStopWorker));
+    renderText(0.9828, 0.33, 0.012, "switching:         " .. tostring(contractorMod.switching));
+    renderText(0.9828, 0.31, 0.012, "passengerLeaving:  " .. tostring(contractorMod.passengerLeaving));
+    renderText(0.9828, 0.29, 0.012, "passengerEntering: " .. tostring(contractorMod.passengerEntering));
+  end
+  -- Restore default alignment (to avoid impacting other mods like FarmingTablet)
+  setTextAlignment(RenderText.ALIGN_LEFT);
+end
+
+-- @doc Capture worker position before switching to another one
+function ContractorModWorker:beforeSwitch(noEventSend)
+  if ContractorModWorker.debug then print("ContractorModWorker:beforeSwitch()") end
+  self.currentVehicle = g_localPlayer:getCurrentVehicle()
+
+  if self.currentVehicle == nil then
+    -- Old passenger condition
+    local passengerHoldingVehicle = g_currentMission.passengerHoldingVehicle;
+    if passengerHoldingVehicle ~= nil then
+      -- source worker is passenger in a vehicle
+    else
+      -- source worker is not in a vehicle
+      self.x, self.y, self.z, self.rotY = g_localPlayer:getPosition()
+      if ContractorModWorker.debug then print("ContractorModWorker: "..tostring(self.x)..", "..tostring(self.y)..", "..tostring(self.z)) end
+      self.rotX = g_localPlayer.rotX;
+      self.rotY = g_localPlayer.rotY;
+-- self.player.isEntered = false
+-- self.player:setStyleAsync(self.playerStyle, nil, noEventSend)
+-- -- self.playerStyle:print()
+-- if noEventSend == nil or noEventSend == false then
+--   -- print("set visible 1: "..self.name)
+--   self.player:setVisibility(true)
+-- end
+      -- if ContractorModWorker.debug then print("ContractorModWorker: moveTo "..tostring(self.player.model.style.playerName)); end
+
+      -- local terrainHeight = getTerrainHeightAtWorldPos(g_currentMission.terrainRootNode, self.x, 300, self.z)
+      -- self.y = math.max(terrainHeight + 0.1, self.y + 0.9)
+    
+      -- self.player:moveRootNodeToAbsolute(self.x, self.y, self.z)
+-- self.player:moveTo(self.x, self.y-0.8, self.z, true, true)
+      g_localPlayer:teleportTo(self.x, self.y, self.z)
+      -- local x, y, z = getWorldTranslation(spawnPoint)
+      -- local dx, _, dz = localDirectionToWorld(spawnPoint, 0, 0, -1)
+      -- local dx, _, dz = localDirectionToWorld(g_currentMission.player.rootNode, 0, 0, -1)
+      -- local ry = MathUtil.getYRotationFromDirection(dx, dz)
+      -- local y = math.max(self.y, getTerrainHeightAtWorldPos(g_currentMission.terrainRootNode, self.x, 0, self.z) + 0.2)
+
+      -- self.player:moveTo(self.x, y, self.z, true, true)
+      -- self.player:setRotation(0, ry)
+      --[[
+      self.player.baseInformation.isOnGround = true
+      self.player:moveToAbsoluteInternal(self.x, self.y, self.z)
+    
+      local dx, _, dz = localDirectionToWorld(g_currentMission.player.rootNode, 0, 0, -1)  --]]
+      -- self.rotY = MathUtil.getYRotationFromDirection(dx, dz)
+    
+      -- setRotation(self.player.graphicsRootNode, 0, self.rotY + math.rad(180.0), 0)
+      -- setRotation(self.player.cameraNode, self.rotX, self.rotY, 0)
+    end
+  else
+    -- source worker is in a vehicle
+    self.x, self.y, self.z = getWorldTranslation(self.currentVehicle.rootNode)
+    self.y = self.y + 2 --to avoid being under the ground
+    self.dx, self.dy, self.dz = localDirectionToWorld(self.currentVehicle.rootNode, 0, 0, 1);
+
+    if noEventSend == nil or noEventSend == false then
+      if ContractorModWorker.debug then print("ContractorModWorker: sendEvent(onLeaveVehicle") end
+      g_localPlayer:leaveVehicle()
+    end
+  end
+end
+
+-- @doc Teleport to target worker when switching
+function ContractorModWorker:afterSwitch(noEventSend)
+  if ContractorModWorker.debug then print("ContractorModWorker:afterSwitch()") end
+  
+  if self.currentVehicle == nil then
+    -- target worker is not in a vehicle
+    if g_localPlayer ~= nil then --g_currentMission.controlPlayer and 
+      -- if ContractorModWorker.debug then print("ContractorModWorker: moveTo "..tostring(g_localPlayer.model.style.playerName)); end
+      -- setTranslation(g_currentMission.player.rootNode, self.x, self.y, self.z);
+      -- g_currentMission.player:moveRootNodeToAbsolute(self.x, self.y-0.2, self.z);
+      g_localPlayer:teleportTo(self.x, self.y, self.z)
+      -- g_localPlayer:setRotation(self.rotX, self.rotY)
+      -- self.player.isEntered = true
+      -- self.player.isControlled = true
+      -- self.player:moveToAbsoluteInternal(0, -200, 0); -- to avoid having player at the same location than current player
+      if ContractorModWorker.debug then print("ContractorModWorker: set visible 0: "..self.name); end
+      -- TODO --self.player:setVisibility(false)
+      if ContractorModWorker.debug then
+        print("ContractorModWorker: setStyleAsync ");
+        -- DebugUtil.printTableRecursively(self.playerStyle, " ", 1, 3)
+      end
+      -- g_localPlayer:setStyleAsync(self.playerStyle, nil, false)
+    end
+
+  else
+    -- if self.isPassenger then
+      -- target worker is passenger
+    -- else
+      -- target worker is in a vehicle
+      if noEventSend == nil or noEventSend == false then
+        if ContractorModWorker.debug then print("ContractorModWorker: sendEvent(VehicleEnterRequestEvent:" ) end
+        -- g_client:getServerConnection():sendEvent(VehicleEnterRequestEvent.new(self.currentVehicle, self.playerStyle, self.farmId));
+        g_localPlayer:requestToEnterVehicle(self.currentVehicle)
+        if ContractorModWorker.debug then print("ContractorModWorker: playerStyle "..tostring(self.playerStyle)) end
+      end
+    -- end
+  end
+end
+
