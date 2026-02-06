c392b6e2c02a92e7f3ec18d66f2f867726a98427
Yumi Modding <yumi@yumi-modding.fr>
2026-02-06 22:39:03 +0100

    Update NPC name

--- scripts/cmFSBaseMission.lua
+++ scripts/cmFSBaseMission.lua
@@ -4,7 +4,9 @@ cmFSBaseMission = {}
 function cmFSBaseMission:pre_setPlayerNickname(player, nickname, userId, noEventSend)
   if ContractorMod.debug then print("cmFSBaseMission:pre_setPlayerNickname "..tostring(nickname)) end
   if ContractorMod.workers then
-    ContractorMod.workers[ContractorMod.currentID].name = nickname
+    worker = ContractorMod.workers[ContractorMod.currentID]
+    worker.name = nickname
+    worker.npc.title = nickname
   end
 end
 FSBaseMission.setPlayerNickname = Utils.prependedFunction(FSBaseMission.setPlayerNickname, cmFSBaseMission.pre_setPlayerNickname)
