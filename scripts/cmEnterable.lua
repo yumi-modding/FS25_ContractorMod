92c3dc6b22461fd0991c6e7e33023ea31d395520
Yumi Modding <yumi@yumi-modding.fr>
2026-02-11 12:43:36 +0100

    Fix wrong player/driver style

--- scripts/cmEnterable.lua
+++ scripts/cmEnterable.lua
@@ -21,17 +21,59 @@ Enterable.onLoad = Utils.appendedFunction(Enterable.onLoad, cmEnterable.app_OnLo
 
 -- @doc Prevent from removing driver character when leaving vehicle if switching between workers
 function cmEnterable:getDisableVehicleCharacterOnLeave(superfunc)
-  if ContractorMod.debug then print("cmEnterable:getDisableVehicleCharacterOnLeave") end
+  if ContractorMod.debug then print("cmEnterable:getDisableVehicleCharacterOnLeave", ContractorMod.workers[ContractorMod.currentID].currentSeat) end
   if ContractorMod.switching then
     -- ContractorMod.switching = false
     if ContractorMod.debug then print("switching return false") end
     return false
   end
-  if ContractorMod.passengerLeaving then
-    ContractorMod.passengerLeaving = false
-    if ContractorMod.debug then print("passengerLeaving return false") end
-    return false
-  end
   return true
 end
 Enterable.getDisableVehicleCharacterOnLeave = Utils.overwrittenFunction(Enterable.getDisableVehicleCharacterOnLeave, cmEnterable.getDisableVehicleCharacterOnLeave)
+
+-- Prevent delete or put default character when AI stops
+function cmEnterable:restoreVehicleCharacter(superfunc)
+  if ContractorMod.debug then print("cmEnterable:restoreVehicleCharacter") end
+	if self.spec_enterable.vehicleCharacter ~= nil then
+    for _, worker in pairs(ContractorMod.workers) do
+      local currentVehicle = ContractorMod:getWorkerVehicle(worker)
+      if currentVehicle ~= nil and currentVehicle == self then
+        -- print("Worker "..worker.name.." controlling vehicle")
+        self:setVehicleCharacter(worker.playerStyle)
+      end
+    end
+  end
+end
+Enterable.restoreVehicleCharacter = Utils.overwrittenFunction(Enterable.restoreVehicleCharacter, cmEnterable.restoreVehicleCharacter)
+
+-- Update correctly characters in vehicles
+function cmEnterable:onPlayerStyleChanged(superfunc,style, userId)
+  if ContractorMod.debug then print("cmEnterable:onPlayerStyleChanged") end
+  for _, worker in pairs(ContractorMod.workers) do
+    local currentVehicle = ContractorMod:getWorkerVehicle(worker)
+    if currentVehicle ~= nil and currentVehicle == self then
+      -- print("Worker "..worker.name.." style changed, update seat character")
+      -- printCallstack()
+      superfunc(self, worker.playerStyle, userId)
+    end
+  end
+end
+Enterable.onPlayerStyleChanged = Utils.overwrittenFunction(Enterable.onPlayerStyleChanged, cmEnterable.onPlayerStyleChanged)
+
+-- Lead to vehicle always controlled, running by themselves
+function cmEnterable:getIsControlled()
+	return self.spec_enterable.isControlled or ContractorMod:isControlledByWorker(self)
+end
+-- Enterable.getIsControlled = Utils.overwrittenFunction(Enterable.getIsControlled, cmEnterable.getIsControlled)
+-- Lead to vehicle always controlled, running by themselves
+
+-- 
+function cmEnterable:getIsInteractive(superfunc, superFunc)
+  -- if ContractorMod.debug then print("cmEnterable:getIsInteractive") end
+  -- Prevent to allow entering as driver
+  if ContractorMod:isControlledByWorker(self) then
+    return false
+  end
+  return superfunc(self, superFunc)
+end
+Enterable.getIsInteractive = Utils.overwrittenFunction(Enterable.getIsInteractive, cmEnterable.getIsInteractive)
