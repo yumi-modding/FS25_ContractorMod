bd9424e00f6a079dc0ef67d93fdc5aaa56227955
Yumi Modding <yumi@yumi-modding.fr>
2026-02-25 13:01:49 +0100

    Fix InGameMenuMapFrame with precisionFarming

--- scripts/cmInGameMenuMapFrame.lua
+++ scripts/cmInGameMenuMapFrame.lua
@@ -14,10 +14,11 @@ function cmInGameMenuMapFrame:onClickSwitchWorker()
 			end
 		end
   end
+	return true
 end
 
-function cmInGameMenuMapFrame:onClickActiveWorker()
-  if ContractorMod.debug then print("cmInGameMenuMapFrame:onClickActiveWorker") end
+function cmInGameMenuMapFrame:onClickActivateWorker()
+  if ContractorMod.debug then print("cmInGameMenuMapFrame:onClickActivateWorker") end
 	if self.currentHotspot ~= nil then
 		if self.currentHotspot:isa(NPCHotspot) then
 			local npc = self.currentHotspot:getNPC()
@@ -30,10 +31,11 @@ function cmInGameMenuMapFrame:onClickActiveWorker()
 			end
 		end
   end
+	return true
 end
 
-function cmInGameMenuMapFrame:onClickInactiveWorker()
-  if ContractorMod.debug then print("cmInGameMenuMapFrame:onClickInactiveWorker") end
+function cmInGameMenuMapFrame:onClickDeactivateWorker()
+  if ContractorMod.debug then print("cmInGameMenuMapFrame:onClickDeactivateWorker") end
 	if self.currentHotspot ~= nil then
 		if self.currentHotspot:isa(NPCHotspot) then
 			local npc = self.currentHotspot:getNPC()
@@ -46,48 +48,44 @@ function cmInGameMenuMapFrame:onClickInactiveWorker()
 			end
 		end
   end
+	return true
 end
 
 function cmInGameMenuMapFrame:setMapInputContext(superfunc, canEnter, canReset, canSellVehicle, canVisit, canSetMarker, removeMarker, canBuy, canSell, canManage)  
   if ContractorMod.debug then print("cmInGameMenuMapFrame:setMapInputContext") end
   superfunc(self, canEnter, canReset, canSellVehicle, canVisit, canSetMarker, removeMarker, canBuy, canSell, canManage)
-    -- DebugUtil.printTableRecursively(g_gui.frames.ingameMenuMapOverview.target.contextActions, " ", 1, 2)
-    print("Current hotspot: "..tostring(self.currentHotspot))
-    -- DebugUtil.printTableRecursively(self.contextActions, " ", 1, 2)
-
-    self.ACTIONS["SWITCH_WORKER"] = 15
-    self.contextActions[InGameMenuMapFrame.ACTIONS.SWITCH_WORKER] = {
-        ["text"] = "button_select",
-        ["callback"] = cmInGameMenuMapFrame.onClickSwitchWorker,
-        ["isActive"] = false
-    }
-    self.ACTIONS["ACTIVE_WORKER"] = 16
-    self.contextActions[InGameMenuMapFrame.ACTIONS.ACTIVE_WORKER] = {
-        ["text"] = "ui_gpsInactive",
-        ["callback"] = cmInGameMenuMapFrame.onClickActiveWorker,
-        ["isActive"] = false
-    }
-    self.ACTIONS["INACTIVE_WORKER"] = 17
-    self.contextActions[InGameMenuMapFrame.ACTIONS.INACTIVE_WORKER] = {
-        ["text"] = "ui_gpsActive",
-        ["callback"] = cmInGameMenuMapFrame.onClickInactiveWorker,
-        ["isActive"] = false
-    }
     self.contextActions[InGameMenuMapFrame.ACTIONS.SWITCH_WORKER].isActive = false
-    self.contextActions[InGameMenuMapFrame.ACTIONS.ACTIVE_WORKER].isActive = false
-    self.contextActions[InGameMenuMapFrame.ACTIONS.INACTIVE_WORKER].isActive = false
+    self.contextActions[InGameMenuMapFrame.ACTIONS.ACTIVATE_WORKER].isActive = false
+    self.contextActions[InGameMenuMapFrame.ACTIONS.DEACTIVATE_WORKER].isActive = false
     -- Enable SWITCH_WORKER only for NPCs
     if self.currentHotspot ~= nil and self.currentHotspot:isa(NPCHotspot) then
 			local npc = self.currentHotspot:getNPC()
 			if npc ~= nil then
         local worker = ContractorMod:getWorkerFromNPC(npc)
         if worker ~= nil then
+          if ContractorMod.debug then print("Worker "..worker.name.." active: "..tostring(worker.active)) end
           self.contextActions[InGameMenuMapFrame.ACTIONS.SWITCH_WORKER].isActive = worker.active
-          self.contextActions[InGameMenuMapFrame.ACTIONS.ACTIVE_WORKER].isActive = worker.active == false
-          self.contextActions[InGameMenuMapFrame.ACTIONS.INACTIVE_WORKER].isActive = worker.active
+          self.contextActions[InGameMenuMapFrame.ACTIONS.ACTIVATE_WORKER].isActive = worker.active == false
+          self.contextActions[InGameMenuMapFrame.ACTIONS.DEACTIVATE_WORKER].isActive = worker.active
         end
       end
     end
 end
 
 InGameMenuMapFrame.setMapInputContext = Utils.overwrittenFunction(InGameMenuMapFrame.setMapInputContext, cmInGameMenuMapFrame.setMapInputContext)
+
+
+function cmInGameMenuMapFrame:onLoadMapFinished(superFunc)
+  if ContractorMod.debug then print("cmInGameMenuMapFrame:onLoadMapFinished") end
+  superFunc(self)
+
+  InGameMenuMapFrame.ACTIONS.SWITCH_WORKER = #self.contextActions + 1
+  InGameMenuMapFrame.ACTIONS.ACTIVATE_WORKER = #self.contextActions + 2
+  InGameMenuMapFrame.ACTIONS.DEACTIVATE_WORKER = #self.contextActions + 3
+  
+  self.contextActions[InGameMenuMapFrame.ACTIONS.SWITCH_WORKER] = { ["callback"] = cmInGameMenuMapFrame.onClickSwitchWorker, ["title"] = g_i18n:getText("action_ContractorMod_SWITCH"), ["isActive"] = false }
+  self.contextActions[InGameMenuMapFrame.ACTIONS.ACTIVATE_WORKER] = { ["callback"] = cmInGameMenuMapFrame.onClickActivateWorker, ["title"] = g_i18n:getText("action_ContractorMod_ACTIVATE"), ["isActive"] = false }
+  self.contextActions[InGameMenuMapFrame.ACTIONS.DEACTIVATE_WORKER] = { ["callback"] = cmInGameMenuMapFrame.onClickDeactivateWorker, ["title"] = g_i18n:getText("action_ContractorMod_DEACTIVATE"), ["isActive"] = false }
+  
+end
+InGameMenuMapFrame.onLoadMapFinished = Utils.overwrittenFunction(InGameMenuMapFrame.onLoadMapFinished, cmInGameMenuMapFrame.onLoadMapFinished)
