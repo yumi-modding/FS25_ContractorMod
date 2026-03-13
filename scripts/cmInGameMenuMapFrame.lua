39973ca3a9cf1c4616a62a5a8fbe2209d535a8ba
Yumi Modding <yumi@yumi-modding.fr>
2026-03-13 17:26:17 +0100

    Doc + prevent starting AI on vehicle without driver

--- scripts/cmInGameMenuMapFrame.lua
+++ scripts/cmInGameMenuMapFrame.lua
@@ -1,5 +1,6 @@
 cmInGameMenuMapFrame = {}
 
+-- Enable to switch worker directly from map menu when clicking on an NPC hotspot
 function cmInGameMenuMapFrame:onClickSwitchWorker()
   if ContractorMod.debug then print("cmInGameMenuMapFrame:onClickSwitchWorker") end
 	if self.currentHotspot ~= nil then
@@ -17,6 +18,7 @@ function cmInGameMenuMapFrame:onClickSwitchWorker()
 	return true
 end
 
+-- Set worker active from the map menu
 function cmInGameMenuMapFrame:onClickActivateWorker()
   if ContractorMod.debug then print("cmInGameMenuMapFrame:onClickActivateWorker") end
 	if self.currentHotspot ~= nil then
@@ -34,6 +36,7 @@ function cmInGameMenuMapFrame:onClickActivateWorker()
 	return true
 end
 
+-- Set worker inactive from the map menu
 function cmInGameMenuMapFrame:onClickDeactivateWorker()
   if ContractorMod.debug then print("cmInGameMenuMapFrame:onClickDeactivateWorker") end
 	if self.currentHotspot ~= nil then
@@ -51,30 +54,66 @@ function cmInGameMenuMapFrame:onClickDeactivateWorker()
 	return true
 end
 
+-- Display switch/activate/deactivate worker actions in map menu only for NPC hotspots with workers and depending on worker state
 function cmInGameMenuMapFrame:setMapInputContext(superfunc, canEnter, canReset, canSellVehicle, canVisit, canSetMarker, removeMarker, canBuy, canSell, canManage)  
   if ContractorMod.debug then print("cmInGameMenuMapFrame:setMapInputContext") end
   superfunc(self, canEnter, canReset, canSellVehicle, canVisit, canSetMarker, removeMarker, canBuy, canSell, canManage)
-    self.contextActions[InGameMenuMapFrame.ACTIONS.SWITCH_WORKER].isActive = false
-    self.contextActions[InGameMenuMapFrame.ACTIONS.ACTIVATE_WORKER].isActive = false
-    self.contextActions[InGameMenuMapFrame.ACTIONS.DEACTIVATE_WORKER].isActive = false
-    -- Enable SWITCH_WORKER only for NPCs
-    if self.currentHotspot ~= nil and self.currentHotspot:isa(NPCHotspot) then
-			local npc = self.currentHotspot:getNPC()
-			if npc ~= nil then
-        local worker = ContractorMod:getWorkerFromNPC(npc)
-        if worker ~= nil then
-          if ContractorMod.debug then print("Worker "..worker.name.." active: "..tostring(worker.active)) end
-          self.contextActions[InGameMenuMapFrame.ACTIONS.SWITCH_WORKER].isActive = worker.active
-          self.contextActions[InGameMenuMapFrame.ACTIONS.ACTIVATE_WORKER].isActive = worker.active == false
-          self.contextActions[InGameMenuMapFrame.ACTIONS.DEACTIVATE_WORKER].isActive = worker.active
-        end
+  self.contextActions[InGameMenuMapFrame.ACTIONS.SWITCH_WORKER].isActive = false
+  self.contextActions[InGameMenuMapFrame.ACTIONS.ACTIVATE_WORKER].isActive = false
+  self.contextActions[InGameMenuMapFrame.ACTIONS.DEACTIVATE_WORKER].isActive = false
+  -- Enable SWITCH_WORKER only for NPCs
+  if self.currentHotspot ~= nil and self.currentHotspot:isa(NPCHotspot) then
+    local npc = self.currentHotspot:getNPC()
+    if npc ~= nil then
+      local worker = ContractorMod:getWorkerFromNPC(npc)
+      if worker ~= nil then
+        if ContractorMod.debug then print("Worker "..worker.name.." active: "..tostring(worker.active)) end
+        self.contextActions[InGameMenuMapFrame.ACTIONS.SWITCH_WORKER].isActive = worker.active
+        self.contextActions[InGameMenuMapFrame.ACTIONS.ACTIVATE_WORKER].isActive = worker.active == false
+        self.contextActions[InGameMenuMapFrame.ACTIONS.DEACTIVATE_WORKER].isActive = worker.active
       end
     end
+  end
 end
-
 InGameMenuMapFrame.setMapInputContext = Utils.overwrittenFunction(InGameMenuMapFrame.setMapInputContext, cmInGameMenuMapFrame.setMapInputContext)
 
+-- Display menu Create job only if driver in the vehicle
+function cmInGameMenuMapFrame:getCanCreateJob(superfunc)
+  if superfunc(self) then
+    if self.currentHotspot ~= nil then
+      local vehicle = self.currentHotspot:getVehicle()
+      return ContractorMod:hasDriver(vehicle)
+    end
+  end
+  return false
+end
+InGameMenuMapFrame.getCanCreateJob = Utils.overwrittenFunction(InGameMenuMapFrame.getCanCreateJob, cmInGameMenuMapFrame.getCanCreateJob)
 
+-- Display menu Go to location only if driver in the vehicle
+function cmInGameMenuMapFrame:getCanGoTo(superfunc)
+  if superfunc(self) then
+    if self.currentHotspot ~= nil then
+      local vehicle = self.currentHotspot:getVehicle()
+      return ContractorMod:hasDriver(vehicle)
+    end
+  end
+  return false
+end
+InGameMenuMapFrame.getCanGoTo = Utils.overwrittenFunction(InGameMenuMapFrame.getCanGoTo, cmInGameMenuMapFrame.getCanGoTo)
+
+-- Display menu Start job only if driver in the vehicle
+function cmInGameMenuMapFrame:getCanStartJob(superfunc)
+  if superfunc(self) then
+    if self.currentHotspot ~= nil then
+      local vehicle = self.currentHotspot:getVehicle()
+      return ContractorMod:hasDriver(vehicle)
+    end
+  end
+  return false
+end
+InGameMenuMapFrame.getCanStartJob = Utils.overwrittenFunction(InGameMenuMapFrame.getCanStartJob, cmInGameMenuMapFrame.getCanStartJob)
+
+-- Append new menu options to map menu for workers
 function cmInGameMenuMapFrame:onLoadMapFinished(superFunc)
   if ContractorMod.debug then print("cmInGameMenuMapFrame:onLoadMapFinished") end
   superFunc(self)
