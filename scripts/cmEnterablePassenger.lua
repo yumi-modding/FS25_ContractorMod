1b3aaec80b1382f6ae2409bde6676bc9c245b7d0
Yumi Modding <yumi@yumi-modding.fr>
2026-03-13 21:20:09 +0100

    Trying to improve passenger management

--- scripts/cmEnterablePassenger.lua
+++ scripts/cmEnterablePassenger.lua
@@ -1,12 +1,16 @@
 cmEnterablePassenger = {}
 
+-- Activate passenger enterable for vehicles with passenger seats
 function cmEnterablePassenger:onLoad(superFunc, savegame)
   if ContractorMod.debug then print("cmEnterablePassenger:onLoad") end
   superFunc(self, savegame)
   -- Force available to true if there are passenger seats (bypass multiplayer check)
   local spec = self.spec_enterablePassenger
-  if spec ~= nil and #spec.passengerSeats > 0 and ContractorMod.enablePassenger then
+  if spec ~= nil and #spec.passengerSeats > 0 then
       spec.available = true
+    --   spec.allowInSingleplayer = true
+    --   spec.allowPassengerOnly = true
+    --   spec.allowVehicleControl = true
       -- Subscribe to message center since available is now true
       if not spec.messageSubscribed then
           g_messageCenter:subscribe(MessageType.PLAYER_STYLE_CHANGED, self.onPassengerPlayerStyleChanged, self)
@@ -16,7 +20,8 @@ function cmEnterablePassenger:onLoad(superFunc, savegame)
 end
 EnterablePassenger.onLoad = Utils.overwrittenFunction(EnterablePassenger.onLoad, cmEnterablePassenger.onLoad)
 
--- Leaving passenger don't leave vehicle leading to crash just after
+-- Value worker.currentSeat when entering/exiting as passenger to keep track of occupied seats
+-- TODO: Leaving passenger don't leave vehicle leading to crash just after
 function cmEnterablePassenger:setPassengerSeatCharacter(superfunc, seatIndex, playerStyle)
     if ContractorMod.debug then print(string.format("cmEnterablePassenger:setPassengerSeatCharacter seatIndex=%s playerStyle=%s", tostring(seatIndex), tostring(playerStyle))) end
     if ContractorMod.switching and playerStyle == nil then
@@ -27,36 +32,125 @@ function cmEnterablePassenger:setPassengerSeatCharacter(superfunc, seatIndex, pl
         if playerStyle ~= nil then
           worker.currentSeat = seatIndex
         else
+          -- TODO: If we leave the vehicle as passenger, we might also clear currentVehicle
+          print("Leaving passenger seat, clear currentSeat for worker "..worker.name)
           worker.currentSeat = nil
         end
         superfunc(self, seatIndex, playerStyle)
     end
+    print(string.format("setPassengerSeatCharacter active camera %s", tostring(self.spec_enterable.activeCamera)))
+
 end
 EnterablePassenger.setPassengerSeatCharacter = Utils.overwrittenFunction(EnterablePassenger.setPassengerSeatCharacter, cmEnterablePassenger.setPassengerSeatCharacter)
 
+-- Enable to enter as passenger if vehicle has free passenger seats
 function cmEnterablePassenger:getIsInteractive(superfunc, superFunc)
---   if ContractorMod.debug then print("cmEnterablePassenger:getIsInteractive") end
     if ContractorMod.enablePassenger then
-        return true
+        if not ContractorMod:isControlledByWorker(self) then
+            return superfunc(self, superFunc)
+        else
+            -- if ContractorMod.debug then print("cmEnterablePassenger:getIsInteractive: true") end
+            return ContractorMod:getFirstFreeSeat(self) > 0 --ContractorMod:hasPassengerSeats(self)
+        end
     else
         return superfunc(self, superFunc)
     end
+
+
+    --     -- If controlled by worker, only allow passenger entry if it has passenger seats
+    --     if ContractorMod.enablePassenger and ContractorMod:isControlledByWorker(self) then
+    --         return ContractorMod:hasPassengerSeats(self)
+    --     end
+    
+    -- -- Not controlled by worker, use normal flow
+    -- return superFunc(self)
 end
 EnterablePassenger.getIsInteractive = Utils.overwrittenFunction(EnterablePassenger.getIsInteractive, cmEnterablePassenger.getIsInteractive)
 
+-- Prevent to switch seat for now since not working correctly and can lead to bugs, to investigate
+function cmEnterablePassenger:actionEventSwitchSeat(superfunc, actionName, inputValue, callbackState, isAnalog, isMouse)
+    if ContractorMod.enablePassenger then
+        print("Switching seat is disabled with ContractorMod to prevent bugs for now")
+        g_currentMission:addIngameNotification(FSBaseMission.INGAME_NOTIFICATION_INFO, g_i18n:getText("ContractorMod_SEAT_SWITCH_DISABLED"))
+        -- TODO: to implement
+    else
+        superfunc(self, actionName, inputValue, callbackState, isAnalog, isMouse)
+    end
+end
+EnterablePassenger.actionEventSwitchSeat = Utils.overwrittenFunction(EnterablePassenger.actionEventSwitchSeat, cmEnterablePassenger.actionEventSwitchSeat)
+
+-- Compute distance to passenger seat to allow entering as passenger if free passenger seats and prevent entering as driver if driver seat occupied, to investigate if we can use it to prevent entering as passenger if no driver in the vehicle
+-- TODO: OK for 1 passenger seat vehicles. Passenger cannot exit vehicle in some cases
 function cmEnterablePassenger:getDistanceToNode(superfunc, superFunc, node)
     local superDistance = superfunc(self, superFunc, node)
     -- quick debug:
-    -- print(("cmEnterablePassenger called for vehicle=%s, superDistance=%.3f"):format(tostring(self:getName()), superDistance))
+    -- if superDistance < 2 then
+    --     print(("cmEnterablePassenger called for vehicle=%s, superDistance=%.3f"):format(tostring(self:getName()), superDistance))
+    -- end
     local isControlledByWorker = ContractorMod and ContractorMod.isControlledByWorker and ContractorMod:isControlledByWorker(self)
     if self:getIsControlled() or self.spec_enterablePassenger.allowPassengerOnly or isControlledByWorker then
         local seatIndex, distance = self:getClosestSeatIndex(node)
+        -- if ContractorMod:getFirstFreeSeat(self) == 0 then
+        --     return math.huge  -- Block all entry
+        -- end
+        -- local seatIndex = ContractorMod:getFirstFreeSeat(self);
+        -- local passengerSeat = self.spec_enterablePassenger.passengerSeats[seatIndex]
+        -- local distance = calcDistanceFrom(node, passengerSeat.node)
+        -- local spec = self.spec_enterablePassenger
+        -- local minDistance = math.huge
+		-- if distance >= spec.minEnterDistance or distance >= minDistance then
+		-- 	continue
+		-- end
+		-- minDistance = distance --self:getClosestSeatIndex(node)
         -- print(("seatIndex=%s distance=%.3f isControlledByWorker=%s"):format(tostring(seatIndex), distance or 0, tostring(isControlledByWorker)))
         if seatIndex ~= nil and distance < superDistance then
             self.interactionFlag = Vehicle.INTERACTION_FLAG_ENTERABLE_PASSENGER
             return distance
         end
+        return math.huge  -- No seats available
     end
     return superDistance
 end
 EnterablePassenger.getDistanceToNode = Utils.overwrittenFunction(EnterablePassenger.getDistanceToNode, cmEnterablePassenger.getDistanceToNode)
+
+-- Take into account occupied seats by workers to determine if passenger seat is available or not
+function cmEnterablePassenger:getIsPassengerSeatIndexAvailable(superfunc, seatIndex)
+    if ContractorMod.enablePassenger and ContractorMod:isControlledByWorker(self) then
+        local seatIndex = ContractorMod:getFirstFreeSeat(self);
+        print(string.format("cmEnterablePassenger:getIsPassengerSeatAvailable seatIndex=%s", seatIndex))
+        return seatIndex ~= nil and seatIndex == seatIndex
+    else
+        return superfunc(self, passengerSeat)
+    end
+end
+EnterablePassenger.getIsPassengerSeatIndexAvailable = Utils.overwrittenFunction(EnterablePassenger.getIsPassengerSeatIndexAvailable, cmEnterablePassenger.getIsPassengerSeatIndexAvailable)
+
+-- We don't want to block AIJob helper if passenger seat is occupied, so return false to not block it
+function cmEnterablePassenger:getIsInUse(superfunc, superFunc, connection)
+	local spec = self.spec_enterablePassenger
+	if spec.available then
+		for seatIndex = 1, #spec.passengerSeats do
+			local passengerSeat = spec.passengerSeats[seatIndex]
+			if not passengerSeat.isUsed then
+				continue
+			end
+			return false    -- At least one passenger seat is occupied, so the enterable is in use but we don't want to prevent using AIJob helper for workers, so return false to not block it
+		end
+		return superfunc(self, superFunc, connection)
+	else
+		return superfunc(self, superFunc, connection)
+	end
+end
+EnterablePassenger.getIsInUse = Utils.overwrittenFunction(EnterablePassenger.getIsInUse, cmEnterablePassenger.getIsInUse)
+
+
+-- Prevent crash in Enterable:postUpdate -> spec.activeCamera:update(dt) but might lead to other bugs like passenger seat blocked by ghost worker, to investigate
+function cmEnterablePassenger:leavePassengerSeat(superfunc, isOwner, seatIndex)
+    print(string.format("cmEnterablePassenger:leavePassengerSeat isOwner=%s seatIndex=%s", tostring(isOwner), tostring(seatIndex)))
+    superfunc(self, isOwner, seatIndex)
+    local specEnterable = self.spec_enterable
+    if specEnterable ~= nil and specEnterable.activeCamera == nil then
+        specEnterable:setActiveCameraIndex(1)
+    end
+end
+EnterablePassenger.leavePassengerSeat = Utils.overwrittenFunction(EnterablePassenger.leavePassengerSeat, cmEnterablePassenger.leavePassengerSeat)
