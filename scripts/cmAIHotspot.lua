e6ce8bbbccdc44453db8d28027b5d1fe1c05d1bd
Yumi Modding <yumi@yumi-modding.fr>
2026-03-13 17:25:16 +0100

    Fix cmAIHotspot:setAIHelperName

--- scripts/cmAIHotspot.lua
+++ scripts/cmAIHotspot.lua
@@ -1,9 +1,19 @@
 -- AIHotspot:setAIHelperName(name)
 cmAIHotspot = {}
 
--- Append character name when worker is active (not really visible)
+-- Append character name when worker is active
 function cmAIHotspot:setAIHelperName(superfunc, name)
     local currentWorker = ContractorMod.workers[ContractorMod.currentID]
+    local vehicle = self:getVehicle()
+    if vehicle ~= nil then
+        for _, worker in pairs(ContractorMod.workers) do
+            local currentVehicle = ContractorMod:getWorkerVehicle(worker)
+            if currentVehicle ~= nil and currentVehicle == vehicle and worker.currentSeat == nil then
+                currentWorker = worker
+                break
+            end
+        end
+    end
     local _, textSize = getNormalizedScreenValues(0, 20)
     self.textSize = textSize
     self:setColor(unpack(currentWorker.color))
